ebf9821365308c915a216e4c233973876e8d7f89
Elmira Aliyeva <37119312+justauser123@users.noreply.github.com>
2019-10-20 22:18:51 +0600

    raid1e

---
+++ raid1e.go
@@ -0,0 +1,24 @@
+package student
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func Raid1e(x, y int) {
+	if x > 0 {
+		for row := 0; row < y; row++ {
+			for column := 0; column < x; column++ {
+				if row == 0 && column == 0 || column == x-1 && row == y-1 && row > 0 && column > 0 {
+					z01.PrintRune('A')
+				} else if row == 0 && column == x-1 || column == 0 && row == y-1 {
+					z01.PrintRune('C')
+				} else if row == 0 || row == y-1 || row <= y-2 && column == 0 || row <= y-2 && column == x-1 {
+					z01.PrintRune('B')
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+			z01.PrintRune('\n')
+		}
+	}
+}
